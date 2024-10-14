0613e46c0109ba3afa1bb5b7c786f6a1e764fd9d
shmaizer <valerashmaizer@gmail.com>
2024-10-14 22:14:33 +0300

    test

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Footer.scss'
-const Footer = () => {
-  return (
-    <footer className='footer'>© 2024-2024 gifby by Shmaizer.</footer>
-  )
-}
-
-export default Footer
--- src/components/UI/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import cl from './Input.scss'
-const Input = (props) => {
-  return (
-    <input {...props}/>
-  )
-}
-
-export default Input
